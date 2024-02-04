1cb8abaeee1c6843f03eb3d727a5b46672c153e9
Purav <puravshah20@gnu.ac.in>
2024-02-04 19:29:53 +0530

    Add dependencies and update routing in App.jsx

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-3xl text-left'>Signup Page</h1>
+    </div>
+  );
+}
+
+export default Signup;
