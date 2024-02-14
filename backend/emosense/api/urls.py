425e8c7a112b28891956c939acfcce3d2c4c833a
Sarjan Patel <77773715+sp190603@users.noreply.github.com>
2024-02-14 21:51:23 +0530

    changes

--- backend/emosense/api/urls.py
+++ backend/emosense/api/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('api/login/',views.login),
     path('api/photo/',views.photo_upload),
     path('api/profile/<int:pk>',views.profile),
+    # path('api/profile/<int:pk>',views.profile),
+    
+    
     
 ]
 
